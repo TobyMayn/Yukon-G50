aa11141ff7e448c592743d9b5e24fd76799ad130
Tobias Mayn <tobiasmayn@gmail.com>
2022-05-06 10:09:31 +0200

    updated version of project

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <string.h>
+#include <cstring>
 #include <stdio.h>
 #include <malloc.h>
 
@@ -23,8 +26,9 @@ struct card {
     char suit;
 };
 
+
 Card *new_card(char rank, char suit){
-    Card *card = malloc(sizeof(Card));
+    Card *card = (Card *)malloc(sizeof(Card));
     card->prev = NULL;
     card->next = NULL;
     card->rank = rank;
@@ -97,6 +101,10 @@ void *save_cards(Card *deck, char* filename){
     fclose(ptr);
 }
 
+void error_message(){
+    printf("\nInvalid move..!\n");
+}
+
 // Helper method for interleave_shuffle
 Card *split_deck(Card *deck, int amount){
     Card *temp_deck = deck;
@@ -104,9 +112,18 @@ Card *split_deck(Card *deck, int amount){
     for (int i = 0; i <= amount; ++i) {
         temp_deck = temp_deck->next;
     }
+    //Emil//Bliver prev og next ikke slettet for den samme node her? (115-117) Ville man ikke skulle gøre sådan her?
+    // [120][100] - [90][70] - [100][130]
+
+    Card *holder = temp_deck->prev;
+    holder->next = NULL;
+    // [120][NULL] - [90][70] - [100][130]
     temp_deck->prev = NULL;
-    Card *part = temp_deck; // Variable for holding split part of deck
-    //temp_deck->next = NULL;
+    // [120][NULL] - [NULL][70] - [100][130]
+
+    temp_deck->prev = NULL;
+    Card *part = temp_deck->next; // Variable for holding split part of deck
+    temp_deck->prev->next = NULL;
     return part;
 }
 
@@ -116,8 +133,8 @@ Card *interleave_shuffle(Card *deck, int amount){
     Card *part = split_deck(deck, amount);
 
     // Add dummy card to new card deck
-    deck = deck->next; // Deck now has first actual card
     new_deck->next = NULL; // Set next pointer to NULL
+    deck = deck->next; // Deck now has first actual card
     //deck->prev = NULL;
 
     while(deck->next != NULL){
@@ -155,9 +172,175 @@ Card *random_shuffle(Card *deck){
     return deck;
 }
 
+//method to find pile ie foundation or column, to start searching for a card.
+//If the given pile doesn't exist the method will return a null pointer.
+Card *get_pile(char rank,char suit){
+    Card *temp = NULL;
+    for (int i = 0; i < ((sizeof(columns) / sizeof(columns[0])) + sizeof(foundations) / sizeof(foundations[0])) ; ++i) {
+        if (i >= 7){
+            if(foundations[i % 7]->rank == rank && foundations[i % 7]->suit == suit)
+                temp = foundations[i % 7];
+        }
+        else if(columns[i % 7]->rank == rank && columns[i % 7]->suit == suit){
+            temp = columns[i];
+            break;
+        }
+    }
+    return temp;
+}
+
+Card *find_card(char rank,char suit,Card *ptr){
+    while(ptr->next != NULL && (ptr->rank != rank && ptr->suit != suit)){
+        ptr = ptr->next;
+    }
+    if(ptr->rank != rank && ptr->suit != suit || ptr->prev == NULL){ //Checks if the specified card, is the dummy card + checking if the card exists
+        error_message();
+        return NULL;
+    }
+    return ptr;
+}
+
+//the foundations will not have a predefined suit. The suit of the foundation will be defined by the first card moved to it.
+bool move_to_foundation(Card *card, Card *toPile) {
+    bool possMove = false;
+    if(card->next != NULL)//checks if we're trying to move more than one card.
+        return possMove;
+
+    if (toPile->prev == NULL){//Checks if the foundation is empty.
+        if(card->rank == ranks[0])//if the foundation is empty check if the card is an Ace.
+            possMove = true;
+    }else{
+        //find topcard in foundation
+        while(toPile->next != NULL){
+            toPile = toPile->next;
+        }
+        int i = 0;
+        while(ranks[i] != card->rank){ //Find the index for the cards rank.
+            i++;
+        }
+
+        if(ranks[i-1] == toPile->rank){//checks if the topcard is one rank less, than the card we with to move.
+            if(card->suit == toPile->suit)//checks for same suit.
+                possMove = true;
+        }
+    }
+    return possMove;
+}
+
+bool valid_move(Card *card,Card *topile){
+    bool valid = false;
+    int i = 0;
+
+    if(topile->rank == foundations[0]->rank){
+        return move_to_foundation(card,topile);
+    }
+
+    while(topile->next != NULL){
+        topile = topile->next;
+    }
+    while(ranks[i] != card->rank){
+        i++;
+    }
+    if(i < 12 && ranks[i+1] == topile->rank){
+        if(card->suit != topile->suit)
+            valid = true;
+    }
+    if(i == 12){
+        //checks if pile is empty for king move
+        if(topile->prev == NULL)
+            valid = true;
+    }
+    return valid;
+}
+
+void move_specific(const char *command,Card *pointer) {
+    //checks if card exists in pile / list
+    pointer = find_card(command[3],command[4],pointer);
+    if(pointer == NULL){
+        error_message();
+        return;
+    }
+
+    //finds the column or foundation specified in the command.
+    Card *to = get_pile(command[7],command[8]);
+    if(to == NULL){
+        error_message();
+        return;
+    }
+
+    while(to->next != NULL){
+        to = to->next;
+    }
+    //from this point we have found the card from a pile, and the pile it's supposed to go to.
+    //now we check if the move is valid.
+    if(valid_move(pointer,to)){
+        //moves card from a stack
+        Card *temp = pointer->prev;
+        temp->next = NULL;
+
+        pointer->prev = to;
+        to->next = pointer;
+    }
+}
+
+
+void pile_to_pile(const char *command,Card *pointer) {
+    Card *to = get_pile(command[4],command[5]);
+    if (to == NULL){
+        error_message();
+        return;
+    }
+
+    while(pointer->next != NULL){
+        pointer = pointer->next;
+    }
+    while(to->next != NULL){
+        to = to->next;
+    }
+
+    Card *temp;
+    if(valid_move(pointer,to)){
+        temp = pointer->prev;
+        temp->next = NULL; //Dereferencing card from old list.
+
+        pointer->prev =  to;
+        to->next = pointer;//Referencing card in new list.
+    }
+
+}
+
+void move(const char *command, int strlen) {
+    Card *temp = get_pile(command[0], command[1]);//gets pointer of list to move from
+    if (temp == NULL) {
+        error_message();//if the get_pile method returns a null pointer we return to caller with error message
+        return;
+    }
+
+    int type = 2;
+    if (strlen == 6)
+        type = 0;
+    else if (strlen == 9)
+        type = 1;
+
+
+    switch (type) {
+        case 0 :
+            pile_to_pile(command, temp);
+            break;
+        case 1:
+            move_specific(command, temp);
+            break;
+        case 2:
+            error_message();
+            return;
+    }
+
+}
+
+
 void show(){
     Card *temp = head;
-    printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+    printf("\n\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
     int i = 0;
     int j = 1;
     int f;
@@ -181,6 +364,25 @@ void show(){
     printf("\n\n");
 }
 
+int find_longest_list(){
+    int max_length = 0;
+    //temp pointer to iterate through lists
+    Card *temp;
+    //finding the longest linked list.
+    for (int i = 0; i < sizeof(columns) / sizeof(columns[0]); ++i) {
+        int j = 0;
+        temp = columns[i];
+        while(temp->next != NULL){
+            temp = temp->next;
+            j++;
+        }
+        if(j > max_length)
+            max_length = j;
+    }
+    return max_length;
+}
+
+//method for printing the contents of each list
 void print_gamestate(){
     Card *placeholder[7];
 
@@ -189,27 +391,20 @@ void print_gamestate(){
         placeholder[i] = columns[i];
     }
 
-    int max_length = 0;
-    //temp pointer to iterate through lists
-    Card *temp;
-    //finding the longest linked list.
-    for (int i = 0; i < sizeof(placeholder) / sizeof(placeholder[0]); ++i) {
-        int j = 0;
-        temp = placeholder[i];
-        while(temp->next != NULL){
-            temp = temp->next;
-            j++;
-        }
-        if(j > max_length)
-            max_length = j;
-    }
+    int max_length = find_longest_list();
+    if(max_length < 8)
+        max_length = 8;
 
-    printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+    printf("\n");
+    for (int i = 0; i < sizeof(placeholder) / sizeof(placeholder[0]); ++i) {//Prints the dummy cards for all columns
+        printf("\t%c%c",placeholder[i]->rank,placeholder[i]->suit);
+    }
+    printf("\n");
     int f = 1;
     Card* foundation_temp;
     //printing all cards in the column or and empty space if there is no card.
     for (int i = 0; i < max_length; ++i) {
-        printf("\n");
+        printf("\n\t");
         for (int j = 0; j < sizeof(placeholder) / sizeof(placeholder[0]); ++j) {
             if (placeholder[j]->next != NULL){
                 placeholder[j] = placeholder[j]->next;
@@ -220,19 +415,17 @@ void print_gamestate(){
         }
         //printing foundation cards for evert second row.
         f++;
-        if (f % 2 == 0 && (f/2) < sizeof(foundations) / sizeof(foundations[0])){
+        if (f % 2 == 0 && ((f/2) - 1) < (sizeof(foundations) / sizeof(foundations[0]))){
             foundation_temp = foundations[(f / 2) - 1];
-            if(foundation_temp->next == NULL){
-                printf("[]\tF%c",(f / 2) + '0');
+            while (foundation_temp->next != NULL) {
+                foundation_temp = foundation_temp->next;
             }
-            else {
-                while (foundation_temp->next != NULL) {
-                    foundation_temp = foundation_temp->next;
-                }
-                printf("%c%c\t",foundation_temp->rank,foundation_temp->suit);
+            if(foundation_temp->prev == NULL){
+                printf("[]\tF%c",(f / 2) + '0');
+            }else{
+                printf("%c%c\t%c%c",foundation_temp->rank,foundation_temp->suit,foundations[(f / 2) - 1]->rank,foundations[(f / 2) - 1]->suit);
             }
         }
-
     }
 
 
@@ -247,16 +440,25 @@ const char* get_input() {
 
 void setup_columns_foundations(){
     for (int i = 0; i < 4; ++i) {
-        foundations[i] = new_card('d', 'd'); //dummy card
+        foundations[i] = new_card('F', i + 1 +'0'); //dummy card
     }
     for (int i = 0; i < 7; ++i) {
-        columns[i] = new_card('d', 'd'); //dummy card
+        columns[i] = new_card('C', i + 1 + '0'); //dummy card
     }
 }
 
+int find_string_length(const char *string){
+    int size = 0;
+    for (int i = 0; i < strlen(string); ++i) {
+        if(string[i] != ' ') {
+            size++;
+        }
+    }
+    return size;
+}
 
 int main() {
-    int a = 4;
+
 //    setup_columns_foundations();
 //    Card *first_card = columns[0];
 //    Card *card  = new_card('A', 'C');
@@ -272,20 +474,35 @@ int main() {
 //    printf("\t  \t  \t  \t  \t  \t  \tKH\t\tF4\n");
 //    printf("\t  \t  \t  \t  \t  \t  \t  \t\t\n");
 
+//test for move method
+/*
+setup_columns_foundations();
+    Card *tempCard = new_card('K','H');
+    columns[4]->next = tempCard;
+    tempCard->prev = columns[4];
+    card *tempcard2 = new_card('A','H');
+    columns[0]->next = tempcard2;
+    tempcard2->prev = columns[0];
+    print_gamestate();
+    char moveF[] = "C5:KH->C6  ";
+    move(moveF, find_string_length(moveF));
+    char moveE[] = "C1->F1";
+    move(moveE, find_string_length(moveE));
+    print_gamestate();
+    */
+
     //Test for show method
-    //head = load_deck("C:\\Users\\emil1\\OneDrive\\Documents\\GitHub\\YukonS-G50\\Test_input.txt");
-    //show();
+    head = load_deck("C:\\Users\\emil1\\OneDrive\\Documents\\GitHub\\YukonS-G50\\Test_input.txt");
+    show();
 
     // Test to print all cards, if no input file is provided
     Card *deck = default_deck();
-    head = interleave_shuffle(deck, 10);
-    show();
 
-    //Card *play_deck = interleave_shuffle(deck, 20);
-//    do {
-//        deck = deck->next;
-//        printf("%c%c\n",deck->rank, deck->suit);
-//    }  while (deck->next != NULL);
+    Card *play_deck = interleave_shuffle(deck, 20);
+    do {
+        play_deck = play_deck->next;
+        printf("%c%c\n",play_deck->rank, play_deck->suit);
+    }  while (play_deck->next != NULL);
 //
 //
 //    do {
